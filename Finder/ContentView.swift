86884f04cd5c80b435c54b63616f536661c3745a
Sushant Suwal <sushantdsuwal@gmail.com>
2024-02-22 15:00:08 +0545

    Initial Commit

---
+++ Finder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finder
+//
+//  Created by Sushant Suwal on 22/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finder/FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderApp.swift
+//  Finder
+//
+//  Created by Sushant Suwal on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
